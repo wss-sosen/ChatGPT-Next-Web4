61fd73c5f5ffcfcbd43bc39159ad571ffa40906f
wss-sosen <127080498+wss-sosen@users.noreply.github.com>
2023-04-03 21:54:12 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -635,7 +635,7 @@ export function Home() {
             构建国内便捷访问的人工智能助手，速度更快，反应更灵敏，功能更全面！更多资讯可关注infoepoch官网www.infoepoch.cn。
           </div>
           <div style={{ display: "flex", alignItems: "center" }}>
-            <img src="图片地址" alt="图片描述" style={{ width: "50%", height: "auto" }} />
+            <img src="https://www.infoepoch.cn/zhishixingqiu-demo.png" alt="图片描述" style={{ width: "50%", height: "auto" }} />
             <div style={{ marginLeft: "20px" }}>
               <p>想要了解更多内容的朋友可以加入我们的知识星球</p>
             </div>

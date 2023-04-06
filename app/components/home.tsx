a60afebf12469516d3074459a22eecad6d3ea4e8
wss-sosen <127080498+wss-sosen@users.noreply.github.com>
2023-04-06 20:08:13 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -666,6 +666,11 @@ export function Home() {
                 <IconButton icon={<GithubIcon />} />
               </a>
             </div>
+            <div className={styles["sidebar-action"]}>
+              <a href={REPO_URL} target="_blank">
+                <IconButton icon={<GithubIcon />} />
+              </a>
+            </div>
           </div>
           <div>
             <IconButton
